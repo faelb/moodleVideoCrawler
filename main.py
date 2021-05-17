3b8f7db13a1438b74ff3e0e183d24fd98064b155
faelb <faelb@gmx.at>
2021-05-17 21:17:35 +0200

    initial commit from Windows

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+File name: main.py
+Author: faelb (faelb@gmx.at)
+Date created: 17/05/2021
+Date last modified: 17/05/2021
+Python Version: 3.9
+This program crawls moodle for (Panopto!)Videos
+and downloads them automatically.
+Attention! This also downloads videos that
+are restricted due to privacy issues - so please take care when using this program.
+UseCase:
+There are around 20 small (Panopto!)Videos on a lecture that is interesting and you assume the worst:
+that this videos won't be available for you any longer when leaving university.
+But downloading each and every video of all your lectures, to store them at your private cloud
+is a pain in the a**. So this small program will do the downloading for you, and rename the video
+just like it is namend on moodle.
+"""
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
